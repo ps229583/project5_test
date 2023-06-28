726a3e6bcf3ef9a06d180c4b1076ab58d571eedb
ps229583 <ps229583@summacollege.nl>
2023-06-28 13:11:09 +0200

    Update oefeningen.blade.php

--- resources/views/oefeningen.blade.php
+++ resources/views/oefeningen.blade.php
@@ -1,33 +1,31 @@
 <x-app-layout>
-<!DOCTYPE html>
-<html lang="nl" xmlns:mso="urn:schemas-microsoft-com:office:office"
-    xmlns:msdt="uuid:C2F41010-65B3-11d1-A29F-00AA00C14882">
+    <!DOCTYPE html>
+    <html lang="nl" xmlns:mso="urn:schemas-microsoft-com:office:office" xmlns:msdt="uuid:C2F41010-65B3-11d1-A29F-00AA00C14882">
 
-<head>
-    <title> Personeel CRUD+</title>
-    <meta charset="utf-8" />
-    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-</head>
+    <head>
+        <title>SummaMove</title>
+        <meta charset="utf-8" />
+        <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+    </head>
 
-<body onload="laad()">
-    <h1>Summa move oefeningen</h1>
+    <body onload="laad()">
+        <h1>Summa move oefeningen</h1>
 
 
-    <br />
-    <table id="oefeningen_tabel">
-        <tr>
-            <th>Naam</th>
-            <th>Beschrijving</th>
-            <th>vertaling</th>
-        </tr>
-        <tbody id="tabelInhoud" />
-    </table>
+        <br />
+        <table id="oefeningen_tabel">
+            <tr>
+                <th>Naam</th>
+                <th>Beschrijving</th>
+                <th>Vertaling</th>
+            </tr>
+            <tbody id="tabelInhoud" />
+        </table>
 
- 
+        <script src="{{ asset('js/summaMove.js') }}"></script>
+    </body>
 
-    <script src="{{ asset('js/summaMove.js') }}"></script>
-</body>
-</html>
+    </html>
 
 
 </x-app-layout>
